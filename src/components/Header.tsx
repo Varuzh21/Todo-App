3ef0ed0a50b0c30a0ab85741a02d480f5d22554f
Varuzh21 <varuzhan-grigoryan@inbox.ru>
2026-02-14 16:15:41 +0400

    i implement header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import Left from '@/assets/icons/left.svg';
+
+import { useNavigation } from '@react-navigation/native';
+
+export function Header({ title }: { title: string }) {
+	const navigate = useNavigation();
+	return (
+		<View style={styles.header}>
+			<TouchableOpacity onPress={() => navigate.goBack()} style={styles.button}>
+				<Left />
+			</TouchableOpacity>
+			<View style={styles.titleContainer}>
+				<Text style={styles.title}>{title}</Text>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	header: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingHorizontal: 18,
+		paddingTop: 30,
+		paddingBottom: 10,
+		minHeight: 80,
+		position: 'relative',
+	},
+	button: {
+		position: 'absolute',
+		left: 18,
+		top: 30,
+		width: 30,
+		height: 30,
+		backgroundColor: '#fff',
+		borderRadius: 50,
+		alignItems: 'center',
+		justifyContent: 'center',
+		zIndex: 10,
+	},
+	titleContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontFamily: 'Medium',
+		fontSize: 25,
+		color: '#fff',
+		textAlign: 'center',
+	},
+});
